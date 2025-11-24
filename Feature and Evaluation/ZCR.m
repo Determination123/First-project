346e1d3d7a6e9c08001b87a934f1fec8a2773787
Determination123 <mbkgnf@163.com>
2025-11-24 22:49:22 +0800

    Delete ZCR.m

--- Feature and Evaluation/ZCR.m
+++
@@ -1,6 +0,0 @@
-function zcr = ZCR(signal)
-    signal = signal(:)'; 
-    signChanges = diff(sign(signal)); % 计算符号差分
-    crossCount = sum(abs(signChanges) ~= 0); 
-    zcr = crossCount / (length(signal) - 1);
-end
